8bebdd876affc3e479d4f27d7d2d5374694f3269
YuryRivera <yury.rivera@gaf.energy>
2024-08-10 14:38:07 -0600

    writnig XML parser serilizer

--- Preprocessor/Parser/XmlParser.cs
+++ Preprocessor/Parser/XmlParser.cs
@@ -9,46 +9,45 @@ public static class XmlParser
     var root = new XmlElementNode("root", [], []);
     using (XmlReader reader = XmlReader.Create(filePath))
     {
-      TraverseDFS(reader, root, reader.Depth);
+      TraverseDFS(reader, root);
     }
 
     return root;
   }
 
-  private static void TraverseDFS(XmlReader reader, XmlElementNode parent, int depth)
+  private static void TraverseDFS(XmlReader reader, XmlElementNode parent)
   {
     while (reader.Read())
     {
-      if (depth >= reader.Depth)
+      switch (reader.NodeType)
       {
-        return;
-      }
-      if (reader.NodeType == XmlNodeType.Element)
-      {
-        var newElmement = new XmlElementNode(reader.Name, [], []);
-        parent.Children.Add(newElmement);
-
-        if (reader.HasAttributes)
-        {
-          while (reader.MoveToNextAttribute())
+        case XmlNodeType.Element:
           {
-            Console.WriteLine($" {reader.Name}={reader.Value}");
+            var newElmement = new XmlElementNode(reader.Name, [], []);
+            parent.Children.Add(newElmement);
+            if (reader.HasAttributes)
+            {
+              while (reader.MoveToNextAttribute())
+              {
+                Console.WriteLine($" {reader.Name}={reader.Value}");
+              }
+              reader.MoveToElement();
+              Console.WriteLine();
+            }
+            if (!reader.IsEmptyElement)
+            {
+              TraverseDFS(reader, newElmement);
+            }
+            break;
           }
-          reader.MoveToElement();
-          Console.WriteLine();
-        }
-
-        if (!reader.IsEmptyElement)
-        {
-          TraverseDFS(reader, newElmement, reader.Depth);
-        }
-        else return;
-      }
-      if (reader.NodeType == XmlNodeType.Text)
-      {
-        var textNode = new XmlElementText(reader.Value);
-        parent.Children.Add(textNode);
-        return;
+        case XmlNodeType.Text:
+          {
+            var textNode = new XmlElementText(reader.Value);
+            parent.Children.Add(textNode);
+            break;
+          }
+        case XmlNodeType.EndElement:
+          return;
       }
     }
   }
--- Preprocessor/Program.cs
+++ Preprocessor/Program.cs
@@ -20,6 +20,25 @@ Console.WriteLine(Environment.CurrentDirectory);
 var filePath = Path.Join("Preprocessor", "template", "investment.svg");
 var rootNode = XmlParser.Parse(filePath);
 
+static void DoSomething(XmlNode n, int level = 0)
+{
+    if (n is XmlElementNode eNode)
+    {
+        Console.WriteLine(eNode.TagName.PadLeft(level));
+        foreach (var child in eNode.Children)
+        {
+            DoSomething(child, level + 2);
+        }
+        return;
+    }
+
+    if (n is XmlElementText tNode) {
+        Console.WriteLine(tNode.Text.PadLeft(level + 1));
+    }
+}
+
+DoSomething(rootNode.Children[0]);
+
 var stream = File.Create("test.pdf");
 renderer.Save(stream);
 stream.Close();
